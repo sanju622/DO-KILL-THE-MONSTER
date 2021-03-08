fdc95ec4b213ce55bb3ddccfc3f2518f06c6c85a
sanju622 <77009363+sanju622@users.noreply.github.com>
2021-03-08 19:08:51 +0530

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,14 @@
+constructor(x,y,r)
+{
+    var options = {
+        density: 1,
+        frictionAir: 1
+    };
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.image=loadImage("Superhero-001.png");
+    this.body=Bodies.circle(this.x,this.y,(this.r)/2,options)
+    World.add(world,this.body);
+
+}
